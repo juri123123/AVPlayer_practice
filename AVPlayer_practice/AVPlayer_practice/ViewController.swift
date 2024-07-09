ffaff1aa582c859e5a31a0851e58de8a5cda8648
JURI <80569323+juri123123@users.noreply.github.com>
2024-07-09 15:17:04 +0900

    initial init

---
+++ AVPlayer_practice/AVPlayer_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVPlayer_practice
+//
+//  Created by 최주리 on 7/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
